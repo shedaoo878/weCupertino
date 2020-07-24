be4e075376575cec16e305ff4638b1586c2778b2
shivanshhedaoo <shivhedaoo7>
2020-07-23 18:03:35 -0700

    hi

--- lib/cards.dart
+++ lib/cards.dart
@@ -1,9 +1,6 @@
-
 import 'package:cupertino_pride/SafeRoutes.dart';
 import 'package:flutter/material.dart';
 import 'package:cupertino_pride/events.dart';
-
-
 
 class cards extends StatelessWidget {
   @override
@@ -11,76 +8,59 @@ class cards extends StatelessWidget {
     return MaterialApp(
       title: "Home Page",
       home: HomeScreen(),
-
     );
     // TODO: implement build
     throw UnimplementedError();
   }
-
-
-
 }
 
-class HomeScreen extends StatefulWidget{
+class HomeScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new homescreen();
-
 }
 
-class homescreen extends State<StatefulWidget>{
+class homescreen extends State<StatefulWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Your Cupertino")
-      ),
+      appBar: AppBar(title: Text("Your Cupertino")),
       body: Container(
         decoration: BoxDecoration(
-            color: Color(0xff537895),
-      ),
+          color: Color(0xff537895),
+        ),
         child: new Center(
           child: new Row(
             children: <Widget>[
               new Padding(padding: new EdgeInsets.all(10.0)),
               new RaisedButton.icon(
-              onPressed: () => {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => SafeRoutes()))
-            },
-
-                  label: Text(
+                onPressed: () => {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SafeRoutes()))
+                },
+                label: Text(
                   'Safe Routes',
-                  ),
-
-                  icon: Icon(Icons.directions_bike),
-                  shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(5.0))),
-          ),
+                ),
+                icon: Icon(Icons.directions_bike),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0))),
+              ),
               new Padding(padding: new EdgeInsets.all(10.0)),
               new RaisedButton.icon(
                 onPressed: () => {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => events()))
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => events()))
                 },
-
                 label: Text(
                   'Events',
-
                 ),
                 icon: Icon(Icons.calendar_today),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(5.0))),
               ),
-        ]
-    )
-    )
-      )
-
+            ],
+          ),
+        ),
+      ),
     );
   }
-
 }
--- lib/events.dart
+++ lib/events.dart
@@ -1,10 +1,5 @@
-
-
 import 'package:cupertino_pride/SafeRoutes.dart';
 import 'package:flutter/material.dart';
-
-
-
 
 class events extends StatelessWidget {
   @override
@@ -12,60 +7,46 @@ class events extends StatelessWidget {
     return MaterialApp(
       title: "Home Page",
       home: HomeScreen(),
-
     );
     // TODO: implement build
     throw UnimplementedError();
   }
-
-
-
 }
 
-class HomeScreen extends StatefulWidget{
+class HomeScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new homescreen();
-
 }
 
-class homescreen extends State<StatefulWidget>{
+class homescreen extends State<StatefulWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text("Events")
+      appBar: AppBar(title: Text("Events")),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color(0xff537895),
         ),
-        body: Container(
-            decoration: BoxDecoration(
-              color: Color(0xff537895),
-            ),
-            child: new Center(
-                child: new Row(
-                    children: <Widget>[
-                      new Padding(padding: new EdgeInsets.all(10.0)),
-                      new RaisedButton.icon(
-                        onPressed: () => {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SafeRoutes()))
-                        },
-
-                        label: Text(
-                          'Event',
-
-                        ),
-                        icon: Icon(Icons.event),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(5.0))),
-                      ),
-          ]
-                      ),
-            )
-
-                )
-            );
-
+        child: new Center(
+          child: new Row(
+            children: <Widget>[
+              new Padding(padding: new EdgeInsets.all(10.0)),
+              new RaisedButton.icon(
+                onPressed: () => {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SafeRoutes()))
+                },
+                label: Text(
+                  'Event',
+                ),
+                icon: Icon(Icons.event),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0))),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
-
 }
