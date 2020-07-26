6da0813fbdb28158906cecc494bfdd0e735683dc
shivanshhedaoo <shivhedaoo7>
2020-07-25 18:56:21 -0700

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 
 import 'package:cupertino_pride/GoogleSignIn.dart';
 import 'package:cupertino_pride/SafeRoutes.dart';
+import 'package:cupertino_pride/Voting.dart';
+import 'package:cupertino_pride/polls.dart';
+import 'package:cupertino_pride/polls.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(GoogleSignInw());
+  runApp(polls());
 }
 
 class MyApp extends StatelessWidget {
--- lib/polls.dart
+++ lib/polls.dart
@@ -1,54 +1,89 @@
-import 'package:cupertino_pride/SafeRoutes.dart';
-import 'package:cupertino_pride/events.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-// ignore: camel_case_types
+
+
+final dummySnapshot = [
+  {"name": "Filip", "votes": 15},
+  {"name": "Abraham", "votes": 14},
+  {"name": "Richard", "votes": 11},
+  {"name": "Ike", "votes": 10},
+  {"name": "Justin", "votes": 1},
+];
+
 class polls extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Home Page",
-      home: HomeScreen(),
+      title: 'Baby Names',
+      home: MyHomePage(),
     );
-    // TODO: implement build
-    throw UnimplementedError();
   }
 }
 
-class HomeScreen extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => new homescreen();
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
 }
 
-class homescreen extends State<StatefulWidget> {
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-          backgroundColor: Colors.blueGrey[900],
-          title: Center(child: Text("Events"))),
-      body: Container(
-        child: new Center(
-          child: new Row(
-            children: <Widget>[
-              new Padding(padding: new EdgeInsets.all(10.0)),
-              new RaisedButton.icon(
-                onPressed: () => {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Events()))
-                },
-                label: Text(
-                  'Event',
-                ),
-                icon: Icon(Icons.event),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(5.0))),
-              ),
-            ],
-          ),
+      appBar: AppBar(title: Text('Baby Name Votes')),
+      body: _buildBody(context),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    // TODO: get actual snapshot from Cloud Firestore
+    return _buildList(context, dummySnapshot);
+  }
+
+  Widget _buildList(BuildContext context, List<Map> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, Map data) {
+    final record = Record.fromMap(data);
+
+    return Padding(
+      key: ValueKey(record.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.votes.toString()),
+          onTap: () => print(record),
         ),
       ),
     );
   }
+}
+
+class Record {
+  final String name;
+  final int votes;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['votes'] != null),
+        name = map['name'],
+        votes = map['votes'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$name:$votes>";
 }
